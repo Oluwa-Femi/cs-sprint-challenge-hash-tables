982d73bca2921eee5d46e8ffdfb95eae49bd9f23
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-07-10 09:53:55 +0100

    exercise 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,31 @@
+from hashtables import (
+    HashTable,
+    hash_table_insert,
+    hash_table_retrieve
+)
+
 def get_indices_of_item_weights(weights, length, limit):
+    ht = HashTable(16)
+
     """
     YOUR CODE HERE
     """
     # Your code here
+    for index, item in enumerate(weights):
+        hash_table_insert(ht, item, index)
+
+    for index, item in enumerate(weights):
+        goal = limit - item
+        lookup = hash_table_retrieve(ht, goal)
+        if lookup is not None:
+            return max(index, lookup), min(index, lookup)
 
     return None
+
+
+def print_answer(answer):
+    if answer is not None:
+        print(str(answer[0] + " " + answer[1]))
+    else:
+        print("None")
+   
